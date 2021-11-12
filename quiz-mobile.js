c7ec6daece964e88c9c4d79c781be3813a66ac55
Tarakyu <worrasong@gmail.com>
2021-11-12 19:23:02 +0900

    edit style

--- quiz-mobile.js
+++ quiz-mobile.js
@@ -30,16 +30,6 @@ let gameSum = 0
 let avg = 0
 
 
-function redirect() {
-    const mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'Windows CE;', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson', 'Mobile', 'Symbian', 'Opera Mobi', 'Opera Mini', 'IEmobile');
-    for (const word in mobileKeyWords){
-        if (navigator.userAgent.match(mobileKeyWords[word]) != null){
-                window.location.href = "./mobile.html";
-            break;
-        }
-    }
-}
-
 
 
 function shareUrl() {
